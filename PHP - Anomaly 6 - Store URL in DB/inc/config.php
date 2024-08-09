c15241216c2e6f466069cd626e67eeae80cd5dea
Marcel Quirijnen <quirijnen.marcel@gmail.com>
2024-08-09 17:06:08 -0500

    remove hardcoded example pwd

--- PHP - Anomaly 6 - Store URL in DB/inc/config.php
+++ PHP - Anomaly 6 - Store URL in DB/inc/config.php
@@ -4,5 +4,5 @@ define("PROJECT_ROOT_PATH", __DIR__ . "/../");
 
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "urashuto");
+define("DB_PASSWORD", "****####****");
 define("DB_DATABASE_NAME", "a6");
